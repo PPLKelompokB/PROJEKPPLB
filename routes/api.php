2a69bfc388276cf4bb55d64c5ea73ec0e2dd9438
VincentAlhoysius <jrmasterps1@gmail.com>
2026-04-24 21:49:09 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EventController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-*/
-
 Route::get('/events/search', [EventController::class, 'index']);
